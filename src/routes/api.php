e830eae69d03521736bdf1c0e4edec85916eb9d0
Willian Leandro <williank10@icloud.com>
2024-09-26 10:51:15 -0300

    fix: add auth exception create user route

--- src/routes/api.php
+++ src/routes/api.php
@@ -5,11 +5,12 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-
 // Login Route
 Route::post('/login', [LoginController::class, 'authenticate']);
-
+// Create User Route
+Route::post('/users', [UserController::class,'store']);
+// Auth Routes
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('/users', UserController::class);
+    Route::apiResource('/users', UserController::class)->except('store');
     Route::apiResource('/expenses', ExpenseController::class);
 });
